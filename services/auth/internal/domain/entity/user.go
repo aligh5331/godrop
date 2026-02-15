b2622b2d369a83d6e8f8f2afd132ad807e4ebf7e
arcan5331 <alighaforian@yahoo.com>
2026-02-16 03:09:55 +0330

    made domain files related to user

---
+++ services/auth/internal/domain/entity/user.go
@@ -0,0 +1,111 @@
+package entity
+
+import (
+	"auth/internal/domain"
+	"strings"
+	"time"
+)
+
+type HashedPassword string
+type User struct {
+	id        string
+	name      string
+	email     string
+	password  HashedPassword
+	active    bool
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+func NewUser(id, name, email string, password HashedPassword, active bool, createdAt time.Time, updatedAt time.Time) (*User, error) {
+
+	var user = &User{
+		id:        strings.TrimSpace(id),
+		name:      strings.TrimSpace(name),
+		email:     strings.TrimSpace(email),
+		password:  password,
+		active:    active,
+		createdAt: createdAt.UTC(),
+		updatedAt: updatedAt.UTC(),
+	}
+
+	if err := user.validate(); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *User) validate() error {
+
+	if u.id == "" {
+		return domain.ErrEmptyId
+	}
+
+	if u.name == "" {
+		return domain.ErrEmptyName
+	}
+
+	if u.email == "" {
+		return domain.ErrEmptyEmail
+	}
+
+	if u.password == "" {
+		return domain.ErrEmptyPassword
+	}
+
+	return nil
+}
+
+func (u *User) SetHashedPassword(p HashedPassword, now time.Time) {
+	u.password = p
+	u.updatedAt = now.UTC()
+}
+
+func (u *User) UpdateProfile(name string, now time.Time) error {
+	newUser := *u
+	newUser.name = strings.TrimSpace(name)
+
+	if err := newUser.validate(); err != nil {
+		return err
+	}
+
+	*u = newUser
+	u.updatedAt = now.UTC()
+	return nil
+}
+
+func (u *User) Activate(now time.Time) {
+	u.active = true
+	u.updatedAt = now.UTC()
+
+}
+func (u *User) Deactivate(now time.Time) {
+	u.active = false
+	u.updatedAt = now.UTC()
+}
+
+func (u *User) Id() string {
+	return u.id
+}
+func (u *User) Name() string {
+	return u.name
+}
+func (u *User) Email() string {
+	return u.email
+}
+func (u *User) IsActive() bool {
+	return u.active
+}
+
+func (u *User) Password() HashedPassword {
+	return u.password
+}
+
+func (u *User) CreatedAt() time.Time {
+	return u.createdAt
+}
+
+func (u *User) UpdatedAt() time.Time {
+	return u.updatedAt
+}
---
+++ services/auth/internal/domain/errors.go
@@ -0,0 +1,35 @@
+package domain
+
+import "errors"
+
+type ValidationError struct {
+	Field   string
+	Message string
+}
+
+func (v ValidationError) Error() string {
+	return v.Message
+}
+
+var (
+	ErrEmptyEmail    = &ValidationError{Field: "email", Message: "email is required"}
+	ErrEmptyPassword = &ValidationError{Field: "password", Message: "password is required"}
+	ErrEmptyName     = &ValidationError{Field: "name", Message: "name is required"}
+	ErrEmptyId       = &ValidationError{Field: "id", Message: "id is required"}
+)
+
+// Auth errors
+var (
+	ErrUserNotFound       = errors.New("user not found")
+	ErrWrongPassword      = errors.New("wrong password")
+	ErrEmailAlreadyExists = errors.New("email already exists")
+)
+
+// Password errors
+
+var (
+	ErrTooLongPassword  = &ValidationError{Field: "password", Message: "password too long"}
+	ErrPasswordTooShort = &ValidationError{Field: "password", Message: "password too short"}
+	ErrPasswordTooWeak  = &ValidationError{Field: "password", Message: "password too weak"}
+	ErrPasswordNotMatch = &ValidationError{Field: "password", Message: "password not match"}
+)
