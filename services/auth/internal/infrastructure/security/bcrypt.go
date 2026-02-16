25479da24a66c56f179f0aab093cd2c3ddc65cb7
arcan5331 <alighaforian@yahoo.com>
2026-02-17 02:17:36 +0330

    implemented Bcrypt Hasher

---
+++ services/auth/internal/infrastructure/security/bcrypt.go
@@ -0,0 +1,38 @@
+package security
+
+import (
+	"auth/internal/domain/entity"
+	"auth/internal/domain/repository"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type BcryptHasher struct {
+	cost int
+}
+
+func NewHasher(cost int) repository.Hasher {
+	if cost < bcrypt.MinCost || cost > bcrypt.MaxCost {
+		cost = bcrypt.DefaultCost
+	}
+	return &BcryptHasher{cost: cost}
+}
+
+func (h *BcryptHasher) Hash(password string) (entity.HashedPassword, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
+	if err != nil {
+		return "", fmt.Errorf("generating bcrypt hash: %w", err)
+	}
+	return entity.HashedPassword(bytes), nil
+}
+
+func (h *BcryptHasher) CheckPasswordHash(password string, hash entity.HashedPassword) bool {
+	if hash == "" {
+		// Perform dummy bcrypt to maintain constant timing
+		_ = bcrypt.CompareHashAndPassword([]byte("$2a$14$dummy"), []byte(password))
+		return false
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
