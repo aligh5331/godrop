cdffbd488277cdb4304273f0a2270c71ce67d1a5
arcan5331 <alighaforian@yahoo.com>
2026-02-17 20:28:12 +0330

    added TokenHasher interface

--- services/auth/internal/domain/repository/hasher.go
+++ services/auth/internal/domain/repository/hasher.go
@@ -6,3 +6,8 @@ type PasswordHasher interface {
 	Hash(password string) (entity.HashedPassword, error)
 	CheckPasswordHash(password string, hashed entity.HashedPassword) bool
 }
+
+type TokenHasher interface {
+	Hash(token string) (entity.HashedToken, error)
+	CheckHash(token string, hashed entity.HashedToken) bool
+}
